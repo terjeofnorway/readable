66adf39199e64488f7dceb8fbc0ea0036a0be5e9
Terje Karlsen <terjeofnorway@gmail.com>
2017-09-12 22:14:12 +0200

    First commit. Getting ready for ejecting create-react-app in order to better automate scss preprosessor.

---
+++ src/containers/StartContainer/index.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+
+class StartContainer extends Component {
+
+    render() {
+
+    }
+
+}
+
+function mapStateToProps ({}) {
+    return {
+
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+    return {
+
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(StartContainer);
+
+export default StartContainer;
